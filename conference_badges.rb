3729bafa8657f1cd850a71fa20e42f28fb0d6bf9
Amanda Rolon <abrolon87@gmail.com>
2019-12-13 18:06:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,10 @@
 # Write your code here.
 def badge_maker(name)
   return "Hello, my name is #{name}."
+end
 
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    return "Hello, my name is #{attendee}"
+  end
 end
